2a9064967df1a289ecb3856e5680717922772161
donatobm <93447434+donatobm@users.noreply.github.com>
2023-06-10 13:34:07 -0400

    Add files via upload

---
+++ ComputacionGrafica/src/graficos/EscribiendoEnMarco.java
@@ -0,0 +1,34 @@
+package graficos;
+import javax.swing.*;
+
+public class EscribiendoEnMarco {
+
+    public static void main (String[]args) {
+    MarcoConTexto mimarco = new MarcoConTexto();
+    mimarco.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+    }
+}
+
+class MarcoConTexto extends JFrame {
+
+    public MarcoConTexto() {
+        setVisible(true);
+        setSize(600,450);
+        setLocation(400,200);
+        setTitle("Primer Texto")
+
+        Lamina miLamina = new Lamina();
+        add(miLamina);
+    }
+} 
+
+class Lamina extends JPanel {
+    public void paintComponent(Graphics g) {
+        super.paintComponent (g);
+        g.drawString("Clase de Computacion",100,100);
+
+
+
+    }
+}
--- ComputacionGrafica/src/module-info.java
+++ ComputacionGrafica/src/module-info.java
@@ -2,7 +2,7 @@
  * 
  */
 /**
- * @author diego
+ * @author donato
  *
  */
 module ComputacionGrafica {
